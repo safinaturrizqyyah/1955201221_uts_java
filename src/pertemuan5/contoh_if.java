bd3238b6e4e76dd24535d2c5a51a0658f78041f3
LENOVO <LENOVO@DESKTOP-4PI23LL>
2020-07-04 20:45:59 +0700

    tugas_uts_java

--- src/pertemuan5/contoh_if.java
+++ src/pertemuan5/contoh_if.java
@@ -32,7 +32,16 @@ public class contoh_if {
         
 //    }
     
+    //Tugas
     public static void main(String[] args) {
+        String nama = JOptionPane.showInputDialog(null, "Siapa Nama Kamu?");
+        String sekolah = JOptionPane.showInputDialog(null, "Asal Sekolah?");
+        String kelas = JOptionPane.showInputDialog(null, "Kamu kelas berapa?");
+        int nilai = Integer.parseInt(JOptionPane.showInputDialog(null, "Berapa Nilai Anda?"));
         
+        if (nilai>90){
+            JOptionPane.showMessageDialog(null, "Selamat " + nama + ", Kamu anak yang cerdas");
+        }   
     }
 }
+
